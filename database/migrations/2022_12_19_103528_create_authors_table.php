9e4a5d2685a8564b932808c9bfc1cd823a265d2b
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2022-12-19 16:59:49 +0300

    create database

--- database/migrations/2022_12_19_103528_create_authors_table.php
+++ database/migrations/2022_12_19_103528_create_authors_table.php
@@ -13,16 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('administration_level')->default(0);
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('authors');
     }
 };
