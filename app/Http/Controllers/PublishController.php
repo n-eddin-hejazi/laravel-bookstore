8f389d02c8390990017cc396fd44adfbf71d7ac5
Nour ALJELANI <89624096+nouraljelani@users.noreply.github.com>
2022-12-19 17:00:54 +0300

    create controllers

---
+++ app/Http/Controllers/PublishController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Publish;
+use Illuminate\Http\Request;
+
+class PublishController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Publish  $publish
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Publish $publish)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Publish  $publish
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Publish $publish)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Publish  $publish
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Publish $publish)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Publish  $publish
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Publish $publish)
+    {
+        //
+    }
+}
